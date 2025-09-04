6948c7e37db093e0eb9fc6ee61263261b26ed40a
Saidbek Kodirov <qodirovsk@gmail.com>
2025-09-04 20:32:47 +0300

    fix: implimented task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const newArray = [];
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 === 0) {
+      newArray.push(...matrix[i]);
+    } else if (i % 2 === 1) {
+      newArray.push(...matrix[i].reverse());
+    }
+  }
+  return newArray;
 };
